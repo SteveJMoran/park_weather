533b4271b8ad4182b0b0cee601a9e1312af0e85c
Steve Moran <stevejamesmoran@gmail.com>
2017-03-04 15:05:44 -0500

    updates obj on api query

--- api.js
+++ api.js
@@ -1,15 +1,18 @@
 var Api = (function () {
 	var url = 'http://api.openweathermap.org/data/2.5/';
 	var appId = "d7ba88cf3ed8a5e0ef3ae4e0e5a1cdba";
-
+	
+	function write(data) {
+		var reportObj = JSON.parse(data);
+		(window.myTrip.weather = window.myTrip.weather || []).push({timestamp : reportObj});
+	}
 	function apiRequest(q) {
 	    var request = new XMLHttpRequest();
 
 	    request.onload = function() {
 	        if (request.readyState == request.DONE ) {
 	           if (request.status == 200) {
-	               var response = request.responseText;
-	               //return response;
+	               write(request.responseText);
 	           }
 	           else if (request.status == 400) {
 	               var response = 'There was an error 400';
@@ -18,12 +21,9 @@ var Api = (function () {
 	               var response = 'something else other than 200 was returned';
 	           }
 	        }
-	        return response;
 	    };
-
 	    request.open("GET", q, true);
 	    request.send();
-	    return request.onload();
 	}
 
 	return {
@@ -37,6 +37,7 @@ var Api = (function () {
 })();
 var App = (function (){
 	var init = function () {
+		window.myTrip = {}
 		//console.log('App');
 	}
 	var currentConditions = function (location) {
@@ -67,60 +68,3 @@ var App = (function (){
 App.init();
 App.current('ottawa');
 App.current(["44.504167","-76.555833"]);
-
-// var Forecast = (function () {
-// 	var fiveday = function() {
-// 		var city = "6094817";
-// 		var con = "http://api.openweathermap.org/data/2.5/forecast?id=" + city + "&units=metric&appid=" + appId;
-// 		request.open("GET", con, false);
-// 		request.send();
-// 		return request.onload();
-// 	}
-// 	var hourly = function(data) {
-// 		var list = data.list;
-// 		var temp = [];
-// 		var coords = [];
-// 		var spread = 0;
-
-// 		for (var i = list.length - 1; i >= 0; i--) {
-// 			//console.log(list[i]);
-// 			temp.push((list[i].main.temp + 40)
-
-// 		);
-
-// 		}
-// 		for (var i = temp.length - 1; i >= 0; i--) {
-
-// 			coords.push(spread +' ' +temp[i]);
-// 			spread = spread + 75;
-// 		}
-// 		trend = document.getElementById('temperature');
-// 		trend.setAttribute("points", coords);
-// 	}
-
-// 	// table set up
-// 	var chart = function(high, low) {
-// 		var chart = document.getElementById('chart');
-// 		var intervalContainer = document.createElement('g');
-// 		intervalContainer.id = 'intervals'
-// 		var spacer = 5;
-// 		var interval = low;
-
-// 		while (interval <= high) {
-// 			point = interval + 75;
-// 			intervalContainer.innerHTML +='<polyline fill="none" stroke="#ddd" stroke-width="0.5" points="00,'+point+' 800,'+point+'" />';
-// 			interval = interval + spacer;
-// 		}
-
-// 		chart.insertBefore(intervalContainer, chart.firstChild);
-// 	}
-
-// 	return {
-// 		init : function() {
-// 			chart(40, -40);
-// 		},
-// 		data: function () {
-// 		  hourly(fiveday());
-// 		}
-// 	};
-
